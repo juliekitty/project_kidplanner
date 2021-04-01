a52863807eba1995a3694447a741ed07661cb962
Julie Landry <julie.landry@gmail.com>
2021-04-01 15:39:05 +0200

    research on timer and periodic for the program detail page

--- lib/views/programDetails.dart
+++ lib/views/programDetails.dart
@@ -5,6 +5,7 @@ import '../src/components/appBar.dart';
 import 'package:project_kidplanner/src/classes/program.dart';
 import 'package:project_kidplanner/resources/programsData.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:async';
 
 class Step1 extends StatelessWidget {
   @override
@@ -74,10 +75,10 @@ class ProgramDetailsView extends StatefulWidget {
 class _ProgramDetailsViewState extends State<ProgramDetailsView> {
   int _selectedScreenIndex = 0;
   List _screens = [
-    {"screen": Step1(), "title": "Kid Planner"},
-    {"screen": Step2(), "title": "Task Timer"},
-    {"screen": Step3(), "title": "my Profile"},
-    {"screen": Step4(), "title": "Advices"},
+    {"screen": Step1(), "title": "Step 1"},
+    {"screen": Step2(), "title": "Step 2"},
+    {"screen": Step3(), "title": "Step 3"},
+    {"screen": Step4(), "title": "Step 4"},
   ];
 
   void _selectScreen(int index) {
@@ -86,10 +87,62 @@ class _ProgramDetailsViewState extends State<ProgramDetailsView> {
     });
   }
 
+  Timer stateTimer;
+
+  void loadPeriodic() {
+    // runs every 5 second
+    stateTimer = Timer.periodic(new Duration(seconds: 2), (timer) {
+      debugPrint(timer.tick.toString());
+      if (_selectedScreenIndex + 1 < _screens.length) {
+        _selectScreen(timer.tick);
+      } else {
+        endTimer();
+      }
+    });
+  }
+
+  void endTimer() {
+    stateTimer?.cancel();
+    Navigator.pop(context);
+  }
+
+  void loadTimer(Duration duration) {
+    Timer timer = new Timer(duration, () {
+      debugPrint("Print after 5 seconds");
+      if (_selectedScreenIndex + 1 < _screens.length) {
+        _selectScreen(_selectedScreenIndex + 1);
+      } else {
+        backState();
+      }
+    });
+  }
+
+  void backState() {
+    Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    //stateTimer.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    // if put here, the function will be called only once
+    // loadPeriodic();
+  }
+
   @override
   Widget build(BuildContext context) {
     final String programType = ModalRoute.of(context).settings.arguments;
     final Program program = findProgramUsingFirstWhere(programs, programType);
+
+    // If not the last step, program a timer to go to the next step
+    print('create a timer');
+    loadTimer(new Duration(seconds: 5));
 
     return Scaffold(
       appBar: appBar(context, Theme.of(context).textTheme,
