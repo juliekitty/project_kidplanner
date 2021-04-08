b45893d286fa88e94ac687d0f33a07f9a5fd8a7a
Julie Landry <julie.landry@gmail.com>
2021-04-08 13:04:42 +0200

    init advices content

--- lib/views/advicesView.dart
+++ lib/views/advicesView.dart
@@ -8,7 +8,141 @@ class AdvicesView extends StatelessWidget {
 //
     return Scaffold(
       appBar: appBar(context, Theme.of(context).textTheme, 'Advices'),
-      body: Text('Advices'),
+      body: Container(
+        color: Colors.yellow[100].withOpacity(0.3),
+        child: PageView(
+            //padding: const EdgeInsets.all(8.0),
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Colors.amber,
+                  child: Center(
+                    child: Text(
+                      'Advices',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.headline3,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Colors.amber,
+                  child: Column(children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 3),
+                      child: Text(
+                        'Limit Keepsakes',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Some children let go of things easily, but for those who are stubborn about saving every little thing, offer up a "limiting container." They can keep all of the keepsakes they want, as long as they fit in a certain box, or on a certain shelf.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Colors.amber,
+                  child: Column(children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 3),
+                      child: Text(
+                        'Stick to a Routine',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'It helps to deliberately verbalize the steps of your morning and evening routines with kids. Post a checklist on a bathroom mirror or bedroom wall for things like packing lunch, gathering homework, and getting dressed. Bedtime habits are important to emphasize because they pave a smoother path to sleep.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Colors.amber,
+                  child: Column(children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 3),
+                      child: Text(
+                        'Ask for Kids\' Help',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'Let kids feel empowered by having them help you plan your errands before leaving the house. Have them help solve the puzzle of pinpointing the fastest route around town — and note logistical roadblocks, like making sure frozen food doesn\'t melt or pets won\'t be left in the car. Make it interesting with a stop for frozen yogurt as a reward.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Colors.amber,
+                  child: Column(children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 3),
+                      child: Text(
+                        'Give Toys a Clear Value',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'The A-B-C-D prioritization tool works for everything: An "A" toy is a favorite one that you love and play with all the time (as often as you eat or brush our teeth). "B" toys are ones you play with a lot (as often as we go to the supermarket). "C" toys are those you don\'t play with very much (as often as we have a birthday or holiday). "D" toys are ones you really are not playing with at all. And D stands for "donate!" Show kids that we want to store our A and B toys where we can reach them and put them away easily, and our C toys up higher in a box or on a shelf.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Card(
+                  color: Colors.amber,
+                  child: Column(children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 20, 10, 3),
+                      child: Text(
+                        'Use Lists',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text(
+                        'You can make packing lists for kids when you are going on a trip, or have them help you make shopping and to-do lists. Kids love to cross things off and you\'re teaching them how to organize their thoughts. Lists can also be helpful for reducing your need to nag when there are several tasks that need to be done.',
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    )
+                  ]),
+                ),
+              ),
+            ]),
+      ),
     );
   }
 }
